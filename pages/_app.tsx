438218b132fd38ab75d137e682b216e989bdea0e
juanjosejjjj <juanjosejjjj@gmail.com>
2022-06-06 17:17:46 -0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CounterProvider } from '../context/Counter'
+import { AuthProvider } from "../context/AuthContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CounterProvider>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </CounterProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
