c52ef54a4d8c862c54fdd6429f4579a37188d42c
juanjosejjjj <juanjosejjjj@gmail.com>
2022-07-10 16:15:12 -0300

    asdas

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,23 +76,31 @@ const Home: NextPage = ({ stars, id, isConnected }: any) => {
   )
 }
 
-
-export async function getStaticProps({ locale }: any) {
-
-  await clientPromise
-  // const { data } = await axios.get<{ id: number, stargazers_count: number }>('https://api.github.com/repos/vercel/next.js')
-  //copia y pega la url en el navegador para ver todo lo q trae ese endpoint
-  //esto es literalmente lo mas util de typescript poder dejar por escrito
-  //los keys y los tipos de datos que trae el endpoint
-  // const stars = data.stargazers_count
-  return {
-    props: {
-      // stars: stars,
-      isConnected: true,
-      ...await serverSideTranslations(locale, ['common']),
-    },
+// export async function getStaticProps({ locale, clientPromise }: any) {
+export async function getServerSideProps({ locale, clientPromise }: any) {
+  try {
+    await clientPromise
+    console.log(clientPromise)
+    // const { data } = await axios.get<{ id: number, stargazers_count: number }>('https://api.github.com/repos/vercel/next.js')
+    //copia y pega la url en el navegador para ver todo lo q trae ese endpoint
+    //esto es literalmente lo mas util de typescript poder dejar por escrito
+    //los keys y los tipos de datos que trae el endpoint
+    // const stars = data.stargazers_count
+    return {
+      props: {
+        // stars: stars,
+        isConnected: true,
+        ...await serverSideTranslations(locale, ['common']),
+      },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
   }
 }
+
 
 
 export default Home
