43cf9cf3c5da00b97e6265468b2282981e8a7d82
juanjosejjjj <juanjosejjjj@gmail.com>
2022-08-24 14:53:19 -0300

    removed counter provider, removed strict in _app.js was not neccesary in nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { CounterProvider } from '../context/Counter'
 import { AuthProvider } from "../context/AuthContext";
 import { appWithTranslation } from 'next-i18next';
-import { StrictMode } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StrictMode>
-      <CounterProvider>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </CounterProvider>
-    </StrictMode>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
 
